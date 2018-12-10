b5ffd26bb79bd55ac48a4d1bc4aad7e39c8209e6
Victor <dzitskiy@gmail.com>
2018-12-11 01:46:12 +0300

    Update Employee.cs

--- Solution/Employee.cs
+++ Solution/Employee.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ConsoleApp6
+namespace ConsoleApp6 // неймспейс
 {
     public class Employee
     {
@@ -14,22 +14,28 @@ namespace ConsoleApp6
             Salary = salary;
             Position = position;
         }
-
+ 
+        // Давай добавим уникальный идентификатор int Id
+        
+        // Хочу три поля для Ф.И.О. и свойства, которое бы их объединяло и возвращал остроку с результатом        
         /// <summary>
         /// Имя сотрудника
         /// </summary>
         public string Name { get; set; }
 
+        // Для этих полей напрашиваются enum c перечислениями, текстовое значение можно подлучать, добавить в enum атрибуты Description 
         /// <summary>
         /// Язык программирования
         /// </summary>
         public string Language { get; set; }
-
-        public int Age { get; set; }
-        public int DepartmentNum { get; set; }
-        public int Salary { get; set; }
         public string Position { get; set; }
+        public int DepartmentNum { get; set; }
+        
+        public int Age { get; set; }
 
+        // ЗП, скорее всего, может быьть дробной, так что напрашивается decimal
+        public int Salary { get; set; }
+        
         public override bool Equals(object obj)
         {
             var employee = obj as Employee;
@@ -42,6 +48,7 @@ namespace ConsoleApp6
                    Position == employee.Position;
         }
 
+        // А этот метод нам для чего?
         public override int GetHashCode()
         {
             var hashCode = -464477449;
