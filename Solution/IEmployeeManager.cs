7ff5ee7e42ff121c85d19e7eb8e49e24bb5e122b
Victor <dzitskiy@gmail.com>
2018-12-11 01:10:55 +0300

    Update IEmployeeManager.cs
    В целом хорошо, не не совсем то, что я имел в виду, видимо, не совсем понятно объяснил (
    
    Я пока не придумал, как лучше полноценное ревью делать, так что буду Pull Request-ом давать общее резюме в описании и  прямо в коде оставлять свои комментарии, а ты их потом можешь убрать, когда будешь исправлять замечания.
    
    Давай немного по архитектуре поговорим, мы же хотим написать консольное приложение так, чтобы потом можно было этот код переиспользовать и перенести в MVC, заменив только отдельные части приложения.
    Идея в том, что каждая часть нашего приложения была макимально независимой, тогда бизнес-логика полностью переедет в MVC и мы сможем легко заменить UI (User Interfece) и чтений из файла на БД.
    Для этого почитай что-нибудь по 3-х уровневой архитектуре приложения, чтоб понимать как мы веделяем слои UI-BLL-DAL? например, https://metanit.com/sharp/mvc5/23.5.php
    В нашем случае:
    UI - классы Program в котором мы выводим меню, всю необходимую информацию о сотрудниках, получаем данные с консоли и передаем их в EmployeeManager (и ничего не знаем о том, как происходит сохранение внутри него, это не задача UI);
    BL - наш EmployeeManager, который выполняет все основные  CRUD-операции с объектами Employee, он никак не должен взаимодействовать с консолью, вся логика по работе с объектами-сотрудника будет добавляться сюда;
    DAL - в нашем случае для консоли это будет чтение\запись из файла, т.е. по хорошему еще бы было неплохо добавить класс-репозиторий (https://metanit.com/sharp/articles/mvc/11.php) для непосредственной работы с файлом, который бы в последствии мы бы заменили на работу с базой.
    
    
    Employee
    https://metanit.com/sharp/articles/mvc/11.php

--- Solution/IEmployeeManager.cs
+++ Solution/IEmployeeManager.cs
@@ -1,9 +1,23 @@
-﻿namespace ConsoleApp6
+//Раз мы все переименовали﻿на Solution, я бы и неймспейс везде переименовал бы (ConsoleApp6 -> Solution)
+namespace ConsoleApp6
 {
     public interface IEmployeeManager
     {
+        // Тут у нас смешиваются два слоя UI И BLL (смоти описание), так быть не должно, такой код будет сложно переделать!
         void AddEmployees();
         void ReadFromFile();
         void SaveToFile();
+        
+        //Я бы хотел тут видеть следующие методы:
+        // List<Employee> ReadAll(); // Получить список всех сотрудников
+        // Employee Read(int Id); // Получить сотрудника по Id
+        // List<Employee> ReadByFilter(EmployeeFilter filter); // Получить список сотрудников по фильтру, добавить класс фильтра (со всемми необходимыми для поиска полями)
+        // void Add(Employee employee); // Добавлене сотрудника
+        // void Delete(int Id); // Удаление сотрудника
+        // void Update(Employee employee); // Изменение сотрудника
+        
+        //SaveToFile - сохранение в файл я бы выполнял неявно, в самих методах Add, Update, Delete
+        
+        //TODO: почитать про generic-коллекции, класс List и интерфейсы IList, IEnumirable и LINQ для фильтра
     }
 }
